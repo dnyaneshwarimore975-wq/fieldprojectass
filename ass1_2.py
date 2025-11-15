cef003860feddc85fad097b84868bde03f2dc09e
dnyaneshwarimore975-wq <dnyaneshwarimore975@gmail.com>
2025-11-15 11:15:12 +0530

    Add files via apload

--- ass1_2.py
+++ ass1_2.py
@@ -1,4 +1,3 @@
-
 #Write a program to input your name, age, and city, 
 # then display them in a formatted sentence.
 
@@ -6,3 +5,4 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 city = input("Enter your city: ")
 print(f" my name is {name}. I am {age} years old and I live in {city}.")
+
