0e858c26e3df78ddfff9b5c6f3520e55673a5922
dnyaneshwarimore975-wq <dnyaneshwarimore975@gmail.com>
2025-11-15 11:14:49 +0530

    Add files via apload

--- ass1_10.py
+++ ass1_10.py
@@ -1,4 +1,3 @@
-
 #Write a program to input the side of a square and display its perimeter and area.
 
 side = float(input("Enter the side of the square: "))
@@ -6,3 +5,4 @@ perimeter = 4 * side
 area = side * side
 print( perimeter)
 print( area)
+
